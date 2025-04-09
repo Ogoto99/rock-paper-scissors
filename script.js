7103f0e31bf786b5ae151229cf826e85c9288855
Ogoto <ogotomelekh99@gmail.com>
2025-04-09 08:25:12 +0300

    added game functionality

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //Get to DOM elements
 const gameContainer = document.querySelector(".container"),
-  userResult = document.querySelector(".user_result img"),
-  cpuResult = document.querySelector(".cpu_result img"),
-  result = document.querySelector(".result"),
-  optionImages = document.querySelectorAll(".option_image");
+userResult = document.querySelector(".user_result img"),
+cpuResult = document.querySelector(".cpu_result img"),
+result = document.querySelector(".result"),
+optionImages = document.querySelectorAll(".option_image");
 
 // Variables to track scores
 let userScore = 0;
